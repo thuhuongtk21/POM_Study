5ed5e6a27fc4b11ad8c7e5f9fdf85448b6551bc5
huongnguyen <huongnguyen@PC-HUONGNGUYEN.vodc.lan>
2018-06-21 16:47:44 +0700

    common functions

---
+++ actions/commons/AbstractTest.java
@@ -0,0 +1,31 @@
+package commons;
+
+import java.util.concurrent.TimeUnit;
+
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.firefox.FirefoxDriver;
+import org.openqa.selenium.ie.InternetExplorerDriver;
+import commons.Constants;
+
+public class AbstractTest {
+	WebDriver driver;
+	
+	  public WebDriver openMultiBrowser(String browser) {
+		  if(browser.equals("firefox")) {
+			  driver = new FirefoxDriver();
+		  }else if(browser.equals("chrome")) {
+			  System.setProperty("webdriver.chrome.driver", ".\\resources\\chromedriver.exe");
+			  driver = new ChromeDriver();
+		  }else {
+			  System.setProperty("webdriver.ie.drivẻ", ".\\resources\\IEDriverServer.exe");
+			  driver = new InternetExplorerDriver();
+		  }
+		  
+		  driver.manage().window().maximize();
+		  driver.get(Constants.URL);
+		  driver.manage().timeouts().implicitlyWait(30, TimeUnit.SECONDS);
+		  return driver;
+	  }
+
+}
---
+++ actions/commons/Constants.java
@@ -0,0 +1,8 @@
+package commons;
+
+public class Constants {
+	public static final String URL = "http://demo.guru99.com/v4/";
+	public static final String UserName = "";
+	public static final String PassWord = "";
+
+}
